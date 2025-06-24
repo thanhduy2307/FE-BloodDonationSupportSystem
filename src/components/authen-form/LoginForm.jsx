3fadee8a2fa38f8cf888ec4c2a935fae617d1306
thanhduy2307 <truongtduy2307@gmail.com>
2025-06-24 08:34:44 +0700

    connect APi

--- src/components/authen-form/LoginForm.jsx
+++ src/components/authen-form/LoginForm.jsx
@@ -17,29 +17,10 @@ const LoginForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // const onFinish = async (values) => {
-  //   try {
-  //     const response = await axios.post('https://ae7d-14-226-226-52.ngrok-free.app/api/Auth/login', values);
-  //     dispatch(login(response.data));
-  //     localStorage.setItem("token", response.data.token);
-  //     localStorage.setItem("user", JSON.stringify(response.data.user));
-  //     // const user = response.data.data;
-  //     // // if (user.role === "ADMIN") {
-  //     // //   navigate("/dashboard/overview");
-  //     // // } else if (user.role === "USER") {
-  //     // //   navigate("/");
-  //     // // }else if (user.role === "STAFF") {
-  //     // //   navigate("/dashboard-staff/user");
-  //     // // }
 
-  //   } catch (e) {
-  //     console.log(e);
-  //     toast.error(e?.response?.data || "Đã xảy ra lỗi khi đăng nhập");
-  //   }
-  // };
 const onFinish = async (values) => {
   try {
-    const response = await axios.post('https://ae7d-14-226-226-52.ngrok-free.app/api/Auth/login', values);
+    const response = await axios.post('https://6568-14-226-226-52.ngrok-free.app/api/Auth/login', values);
 
     const { token, user } = response.data || {};
 
@@ -47,23 +28,21 @@ const onFinish = async (values) => {
       toast.error("Login không thành công: thiếu token hoặc user");
       return;
     }
-
-    // Chỉ toast thành công
     toast.success("Đăng nhập thành công!");
+    
 
-    // Lưu lại token và user nếu muốn
     dispatch(login(user));
     localStorage.setItem("token", token);
     localStorage.setItem("user", JSON.stringify(user));
-     user = response.data.data;
-      if (user.role === "1") {
+    
+      if (user.role == "2") {
         navigate("/dashboard/overview");
-      } else if (user.role === "2") {
+      } else if (user.role == "1") {
         navigate("/");
-      }else if (user.role === "3") {
+      }else if (user.role == "3") {
         navigate("/dashboard-staff/user");
       }
-    // Không chuyển trang
+  
   } catch (e) {
     console.log("Login error:", e);
     toast.error(e?.response?.data?.message || "Đã xảy ra lỗi khi đăng nhập");
