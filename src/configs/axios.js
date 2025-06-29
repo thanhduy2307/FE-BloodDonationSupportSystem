516bc4a79c7286782033f973970a7d6dd6f7f33f
sanglata33 <sanglata332255@gmail.com>
2025-06-29 23:57:32 +0700

    update

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://b30f-222-253-95-253.ngrok-free.app/api/",
+  baseURL: "https://1db2-222-253-95-253.ngrok-free.app/api/",
   headers: {
      'ngrok-skip-browser-warning': 'true',
     Accept: "application/json", // 👈 THÊM DÒNG NÀY
